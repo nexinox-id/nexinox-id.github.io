c6ebb6770da219ebcfd59578d5073ef304d21258
mblonyox <mblonyox@gmail.com>
2025-01-22 21:34:26 +0700

    fix: add description to Peta Kuliner

--- peta.page.ts
+++ peta.page.ts
@@ -1,5 +1,6 @@
 export const renderOrder = 1;
 export const title = "Peta Kuliner";
+export const description = "Peta kuliner NexInox adalah panduan interaktif yang menampilkan lokasi-lokasi makan yang telah dikunjungi oleh komunitas NexInox. Setiap titik di peta mewakili restoran, kafe, warung, atau kedai yang menawarkan pengalaman kuliner unik dan menggugah selera. Peta ini mencakup berbagai jenis masakan, dari hidangan lokal Indonesia hingga makanan internasional, dengan fokus pada kualitas rasa, suasana, dan inovasi kuliner."
 export const layout = "pages/peta.vto";
 
 export default function*({ search }: Lume.Data) {
